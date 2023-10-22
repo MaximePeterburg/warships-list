65e3e7eea9dc4be918ec0c143731a92c1570692b
MaximePeterburg <getmax4444@gmail.com>
2023-10-22 21:05:48 +0300

    fixed type erros in warship card

--- src/components/WarshipCard.tsx
+++ src/components/WarshipCard.tsx
@@ -6,46 +6,36 @@ type WarshipCardProps = {
 };
 
 const WarshipCard = ({ warship }: WarshipCardProps) => {
-  const {
-    description,
-    icons: { medium: vehicleIconURL },
-    title,
-    type: {
-      title: typeTitle,
-      icons: { default: typeIconURL }
-    },
-    nation: {
-      icons: { small: nationIconURL },
-      name: nationName
-    },
-    level
-  } = warship;
   return (
     <li className='px-4 py-2 max-w-xs  h-min  bg-teal-800  relative '>
-      <img src={vehicleIconURL} width='288' height='170' alt={title} />
+      <img src={warship.icons?.medium} width='288' height='170' alt={warship.title} />
       <header className='flex absolute top-2 '>
         <div className='relative'>
           <img
             className='opacity-60'
             height='72'
             width='117'
-            src={nationIconURL}
-            alt={nationName}
+            src={warship.nation?.icons?.small}
+            alt={warship.nation?.title}
           />
-          <img className='absolute top-0 ' src={typeIconURL} alt={typeTitle} />
+          <img
+            className='absolute top-0 '
+            src={warship.type?.icons?.default}
+            alt={warship.type?.title}
+          />
           <p className='absolute text-gray-100 top-0 left-7 font-semibold'>
-            {convertToRoman(level || 0)}
+            {convertToRoman(warship.level || 0)}
           </p>
         </div>
         <h2 className='text-2xl text-center text-gray-100 font-semibold'>
-          {title.toUpperCase()}
+          {warship.title.toUpperCase()}
         </h2>
       </header>
       <details>
         <summary className='text-gray-100 list-none cursor-pointer underline'>
           Описание коробля
         </summary>
-        <p className='italic text-gray-100'>{description}</p>
+        <p className='italic text-gray-100'>{warship.description}</p>
       </details>
     </li>
   );
